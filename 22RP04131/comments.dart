51cf10fd38fd639d25821d941547c5dbd5159b77
ParfaitShema <ivancyusa1@gmail.com>
2025-06-09 14:28:12 +0200

    Work 1 on 09/06/2025

---
+++ 22RP04131/comments.dart
@@ -0,0 +1,17 @@
+void main() {
+  // This is a single-line comment
+
+  /*
+    This is a multi-line comment
+    You can write notes here
+    It will not affect the program
+  */
+
+  // Declare a name
+  String name = "Parfait";
+
+  // Print a greeting
+  print("Hello, $name!");
+
+  // this is how to make comments
+}
