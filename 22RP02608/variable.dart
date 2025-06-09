6796737ca4ca4620adb930d57c2c3b507f7b6ae4
canirinyanadianee <canirinyanadiane@gmail.com>
2025-06-09 14:53:35 +0200

    creating variables

---
+++ 22RP02608/variable.dart
@@ -0,0 +1,15 @@
+void main() {
+  // Variable declaration
+  var name = 'Alice';       // type inferred as String
+  int age = 25;             // explicit type
+  double height = 5.9;
+  bool isStudent = true;
+  dynamic anything = 'Hello'; // can change type later
+
+  // Printing variables
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: $height');
+  print('Is Student: $isStudent');
+  print('Dynamic variable: $anything');
+}
