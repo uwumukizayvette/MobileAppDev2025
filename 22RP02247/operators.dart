a0544cd761a8ba7340011c9cb872674aeeda11de
nyirimbibi <piscos430@gmail.com>
2025-06-09 14:37:38 +0200

    variables,comments,operators and control flow Task

---
+++ 22RP02247/operators.dart
@@ -0,0 +1,17 @@
+void main() {
+  int a = 5;
+  int b = 4;
+
+  print("Addition: ${a + b}");
+  print("Subtraction: ${a - b}");
+  print("Multiplication: ${a * b}");
+  print("Division: ${a / b}");
+  print("Modulus: ${a % b}");
+
+  
+  bool isTrue = true;
+  bool isFalse = false;
+  print("AND: ${isTrue && isFalse}");
+  print("OR: ${isTrue || isFalse}");
+  print("NOT: ${!isTrue}");
+}
---
+++ 22RP02247/variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = "pascal";
+  int age = 100;
+  double height = 2;
+  bool isOld = true;
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Is Student: $isStudent");
+}
