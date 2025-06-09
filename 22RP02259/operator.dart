7be5506bf4b94ed88536c17e960b369631e75848
aimableshyaka <shyakaaimable07@gmail.com>
2025-06-09 13:31:45 +0200

    Operators

---
+++ 22RP02259/operator.dart
@@ -0,0 +1,22 @@
+void main() {
+  int a = 10;
+  int b = 5;
+
+  // Arithmetic Operators
+  print('Addition: ${a + b}');
+  print('Subtraction: ${a - b}');
+  print('Multiplication: ${a * b}');
+  print('Division: ${a / b}');
+  print('Modulus: ${a % b}');
+
+  // Comparison Operators
+  print('a > b: ${a > b}');
+  print('a == b: ${a == b}');
+
+  // Logical Operators
+  bool x = true;
+  bool y = false;
+  print('x && y: ${x && y}');
+  print('x || y: ${x || y}');
+  print('!x: ${!x}');
+}
