54694adf9645da8dc1acce5574db1496a0f0fea5
DinelauMutoni <umutonicocose@gmail.com>
2025-06-09 15:01:51 +0200

    type testing operators is,is ,as

--- 22RP02981/typetestoperator.dart
+++ 22RP02981/typetestoperator.dart
@@ -1,15 +1,18 @@
 void main() {
-  dynamic value1 = 'Claudine';
-  dynamic value2 = 25;
-  dynamic value3 = 3.14;
+  dynamic data = 'Mutoni';
 
   // Using 'is'
-  print('value1 is String: ${value1 is String}'); // true
-  print('value2 is int: ${value2 is int}');       // true
-  print('value3 is double: ${value3 is double}'); // true
+  if (data is String) {
+    print('data is a String'); // true
+  }
 
   // Using 'is!'
-  print('value1 is! int: ${value1 is! int}');     // true
-  print('value2 is! String: ${value2 is! String}'); // true
-  print('value3 is! bool: ${value3 is! bool}');   // true
+  if (data is! int) {
+    print('data is NOT an int'); // true
+  }
+
+  // Using 'as' to cast
+  dynamic someValue = 'Claudine';
+  String name = someValue as String;
+  print('Name after casting: $name');
 }
