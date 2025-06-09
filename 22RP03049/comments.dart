cf0638c3352470ab86bc216e11bc4ba50416d339
Jonathanyzx <jonathannsengimana121@gmail.com>
2025-06-09 16:02:17 +0200

    this coment program

---
+++ 22RP03049/comments.dart
@@ -0,0 +1,14 @@
+void main() {
+  // This is a single-line comment
+
+  /*
+    This is a
+    multi-line comment
+  */
+
+  /// This is a documentation comment used for
+  /// describing functions or classes
+
+  int a = 5; // Variable declaration
+  print(a); // Output the value of a
+}
