d606c46445ce4594ec76ee39e452ced2c354ba94
cldnuwase <=>
2025-06-09 14:13:19 +0200

    variable

---
+++ 22RP02496/variable.dart
@@ -0,0 +1,13 @@
+//using var
+void main(){
+var name = 'claudine';
+var age = 25;
+
+print('My name is $name and I am $age years old.');
+//using explicit type
+String name2 = 'Uwase';
+int year =2026;
+bool isStudent = true;
+print('My name is $name2 and I will graduate in $year.');
+
+}
