993385733857bbacddc1ec1eea8ffc6b3c098ac4
Sandrine-Ira <irashimwakmsandrine@gmail.com>
2025-06-09 17:04:11 +0200

    09 june

--- 22RP03352/contlorflow.dart
+++ 22RP03352/contlorflow.dart
@@ -8,4 +8,36 @@ void main() {
   } else {
     print('You are a minor.');
   }
+
+//You can also chain with else if:
+ int score=80 ;
+if (score >= 90) {
+  print('A');
+} else if (score >= 80) {
+  print('B');
+} else {
+  print('C or lower');
+}
+
+
+//2. Ternary Operator
+  String status = (age >= 18) ? 'Adult' : 'Minor';
+  print('Status: $status');
+
+  //3. Switch Statements
+ String grade = 'B';
+
+  switch (grade) {
+    case 'A':
+      print('Excellent!');
+      break;
+    case 'B':
+      print('Good job!');
+      break;
+    case 'C':
+      print('Needs improvement.');
+      break;
+    default:
+      print('Invalid grade');
+  }
 }
