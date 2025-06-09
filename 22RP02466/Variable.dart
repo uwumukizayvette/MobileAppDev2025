2bfc480c0ac1adfef036d82c32af22b847a7e16f
CynthiaGihozo12345 <igihozoc16@gmail.com>
2025-06-09 13:21:33 +0200

    Variables

---
+++ 22RP02466/Variable.dart
@@ -0,0 +1,8 @@
+void main() {
+    //Variables are used to store data
+  var name = 'Cynthia'; // Type inferred as String
+  int age = 22;         // Store Integer Number
+  double height = 7.4;  // Decimal number
+  bool isStudent = true; // Boolean
+  print('Name: $name, Age: $age, Height: $height, Student: $isStudent');
+}
