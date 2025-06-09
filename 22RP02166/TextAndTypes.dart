a674443a9469802a84eda68d41378a9fbbaf1ade
gaele-shila <ishimwegaelle1@gmail.com>
2025-06-09 13:43:38 +0200

    today's work

---
+++ 22RP02166/TextAndTypes.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = 'Aimable';
+  int age = 22;
+  double height = 5.9;
+  bool isStudent = true;
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: $height feet');
+  print('Is a student: $isStudent');
+}
