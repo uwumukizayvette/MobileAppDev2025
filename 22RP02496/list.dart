75f433eb6b16b043c3b1d214f5386199404204b0
cldnuwase <cldnuwase@gmail.com>
2025-06-09 16:38:47 +0200

    file changed

--- 22RP02496/list.dart
+++ 22RP02496/list.dart
@@ -4,5 +4,6 @@ void main(){
 List<String> fruits = ['Apple', 'Banana', 'Mango'];
 print(fruits[0]); // Output: Apple
 fruits.add('Orange'); // Add an item
+fruits.removeAt(0);
 print(fruits); // Output: [Apple, Banana, Mango, Orange]
 }
