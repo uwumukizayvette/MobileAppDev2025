7fb820e8edf975a1c7aa9d5fae11294d17161412
murezi740 <herveishimwe740@gmail.com>
2025-06-09 14:59:12 -0700

    first work

---
+++ 22RP03130/comment.dart
@@ -0,0 +1,12 @@
+
+    void main() {
+  // This is a single-line comment
+
+  /*
+   This is a
+   multi-line comment
+  */
+
+  print("Comments are ignored by the compiler");
+}
+
---
+++ 22RP03130/operator.dart
@@ -0,0 +1,22 @@
+void main(){
+   
+  int a = 10;
+  int b = 5;
+
+  // Arithmetic
+  print(a + b); // 15
+  print(a - b); // 5
+  print(a * b); // 50
+  print(a / b); // 2.0
+
+  // Comparison
+  print(a > b);  // true
+  print(a == b); // false
+
+  // Logical
+  bool x = true;
+  bool y = false;
+  print(x && y); // false
+  print(x || y); // true
+}
+
