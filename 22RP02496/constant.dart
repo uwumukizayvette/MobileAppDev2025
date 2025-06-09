c80adb367523cb3740f9cf51e55bbb8e053e874c
cldnuwase <cldnuwase@gmail.com>
2025-06-09 19:43:56 +0200

    Constant

---
+++ 22RP02496/constant.dart
@@ -0,0 +1,8 @@
+// const keyword is used to define compile-time constants—values that are known and fixed when the code is compiled and will never change at runtime.
+
+void main() {
+  const double pi = 3.14159;
+print(pi);
+
+
+}
