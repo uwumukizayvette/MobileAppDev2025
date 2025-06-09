68d7554f81e625a874247bf51c47a704be6829e9
isniyomukiza <isniyomukiza@gmail.com>
2025-06-09 16:19:17 +0200

    Update switch.dart

--- Ismael-22RP04197/switch.dart
+++ Ismael-22RP04197/switch.dart
@@ -1,3 +1,13 @@
+/*
+switch compares the value of day against each case.
+
+Each case has a value to match and a block of code to run if matched.
+
+The break statement stops the switch from checking further cases.
+
+default runs if no case matches, like an "else" for switch.
+*/
+
 // Example of Switch Statement
 void main() {
   int day = 2;
