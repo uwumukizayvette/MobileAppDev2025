c9f24019676e841871cc5cbcad833af8576157d4
CynthiaGihozo12345 <igihozoc16@gmail.com>
2025-06-09 16:19:59 +0200

    DataStructure

--- 22RP02466/Hello.dart
+++ 22RP02466/Hello.dart
@@ -1,3 +1,13 @@
 void main(){
-    print("Hello ,World");
+    int a=5;
+    int b=10;
+    
+    if(a>b){
+        String result="a is greater";
+        print(result);
+    }
+    else{
+          String result="b is greater";
+        print(result);
+    }
 }
