2fae64965d7128e35e85314a4c8ad6be9dfdf6fb
isniyomukiza <isniyomukiza@gmail.com>
2025-06-09 15:37:47 +0200

    Work1

---
+++ Ismael-22RP04197/operators.dart
@@ -0,0 +1,27 @@
+void main() {
+  // Arithmetic Operators
+  int a = 10;
+  int b = 3;
+
+  print('a + b = ${a + b}'); // Addition
+  print('a - b = ${a - b}'); // Subtraction
+  print('a * b = ${a * b}'); // Multiplication
+  print('a / b = ${a / b}'); // Division (double)
+  print('a ~/ b = ${a ~/ b}'); // Integer division
+  print('a % b = ${a % b}'); // Modulus (remainder)
+
+  // Relational Operators
+  print('a == b: ${a == b}'); // Equal to
+  print('a != b: ${a != b}'); // Not equal to
+  print('a > b: ${a > b}');   // Greater than
+  print('a < b: ${a < b}');   // Less than
+  print('a >= b: ${a >= b}'); // Greater or equal
+  print('a <= b: ${a <= b}'); // Less or equal
+
+  // Logical Operators
+  bool x = true;
+  bool y = false;
+
+  print('x && y: ${x && y}'); // Logical AND
+  print('!x: ${!x}');         // Logical NOT
+}
