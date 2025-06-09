99736d928b856cd005ab3ffcf67a948886c9a726
cldnuwase <cldnuwase@gmail.com>
2025-06-09 14:38:21 +0200

    Variable

--- 22RP02496/variable.dart
+++ 22RP02496/variable.dart
@@ -7,7 +7,6 @@ print('My name is $name and I am $age years old.');
 //using explicit type
 String name2 = 'Uwase';
 int year =2026;
-bool isStudent = true;
 print('My name is $name2 and I will graduate in $year.');
 
 }
