3efd0ddb01fb57ed5e975e0919b834e6a96b6cdf
isniyomukiza <isniyomukiza@gmail.com>
2025-06-09 16:55:45 +0200

    First Work in Dart

--- Ismael-22RP04197/operators.dart
+++ Ismael-22RP04197/operators.dart
@@ -1,27 +1,28 @@
 void main() {
+  // Variables
+  String name = 'Alice';
+  int age = 25;
+  double height = 1.68;
+  bool isActive = true;
+  
   // Arithmetic Operators
-  int a = 10;
-  int b = 3;
-
-  print('a + b = ${a + b}'); // Addition
-  print('a - b = ${a - b}'); // Subtraction
-  print('a * b = ${a * b}'); // Multiplication
-  print('a / b = ${a / b}'); // Division (double)
-  print('a ~/ b = ${a ~/ b}'); // Integer division
-  print('a % b = ${a % b}'); // Modulus (remainder)
-
-  // Relational Operators
-  print('a == b: ${a == b}'); // Equal to
-  print('a != b: ${a != b}'); // Not equal to
-  print('a > b: ${a > b}');   // Greater than
-  print('a < b: ${a < b}');   // Less than
-  print('a >= b: ${a >= b}'); // Greater or equal
-  print('a <= b: ${a <= b}'); // Less or equal
-
+  int nextYearAge = age + 1;          // + addition
+  double doubleHeight = height * 2;   // * multiplication
+  int yearsUntil30 = 30 - age;        // - subtraction
+  
+  // Comparison Operators
+  bool isAdult = age >= 18;           // >= greater than or equal to
+  
   // Logical Operators
-  bool x = true;
-  bool y = false;
-
-  print('x && y: ${x && y}'); // Logical AND
-  print('!x: ${!x}');         // Logical NOT
+  bool canParticipate = isActive && isAdult; // && logical AND
+  
+  // Print everything
+  print('Hello, my name is $name.');
+  print('I am $age years old.');
+  print('Next year, I will be $nextYearAge years old.');
+  print('My height is $height meters, but double that would be $doubleHeight meters.');
+  print('I will turn 30 in $yearsUntil30 years.');
+  print('Is active: $isActive');
+  print('Is an adult: $isAdult');
+  print('Can participate: $canParticipate');
 }
