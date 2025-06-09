9a00d357dba0ba83767433d16586d0ac49e062ea
CynthiaGihozo12345 <igihozoc16@gmail.com>
2025-06-09 14:24:39 +0200

    TypeTestOperators

---
+++ 22RP02466/TypeTestOperators.dart
@@ -0,0 +1,18 @@
+void main() {
+  // Dynamic variable that holds a String value
+  dynamic data = 'Hello, Dart!';
+
+  // === Using 'is' to check type ===
+  if (data is String) {
+    print("✅ 'data' is a String.");
+  }
+
+  // === Using 'is!' to check if NOT a type ===
+  if (data is! int) {
+    print("✅ 'data' is NOT an int.");
+  }
+
+  // === Using 'as' to cast the type ===
+  String message = data as String;
+  print("Uppercased message: ${message.toUpperCase()}");
+}
