6f61eef167f5ad65ed28676e6b7ce3c83153f997
cldnuwase <cldnuwase@gmail.com>
2025-06-09 20:15:48 +0200

    late variable

---
+++ 22RP02496/ControlFlow.dart
@@ -0,0 +1,13 @@
+// If-Else Statements
+void main(){
+  int age = 18;
+if (age >= 18) {
+  print('Adult');
+} else {
+  print('Minor');
+}
+// Output: Adult
+// The if-else statement checks if the age is 18 or older and prints 'Adult' if true, otherwise 'Minor'.
+
+
+}
---
+++ 22RP02496/lateVariables.dart
@@ -0,0 +1,7 @@
+//Use late when you want to declare a variable but assign it later.
+void main(){
+late String message;
+message = 'Hello Students!';
+print(message); // Output: Hello Dart!
+
+}
