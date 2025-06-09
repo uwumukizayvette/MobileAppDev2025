5bf771a0e730d15d9ba3ce6eff2768d57200d2fe
MGviii <ntwarimugem26@gmail.com>
2025-06-09 13:59:42 +0200

    class activity commit

---
+++ 22RP03758/ControlFlow.dart
@@ -0,0 +1,65 @@
+/*control flow is essential in Dart it allows your program to make decisions, 
+repeat actions, and handle conditions.*/
+
+
+void main() {
+  /*1. Conditional statements
+-------------------------
+"if" Executes a block of code only if a given condition is true.
+ "else if" Checks another condition if the previous if condition was false.
+"else" Executes a block of code if none of the if or else if conditions are true.
+*/ 
+  int score = 75;
+
+  if (score >= 90) {
+    print('Excellent!');
+  } else if (score >= 70) {
+    print('Good job!');
+  } else {
+    print('Keep trying!');
+  }
+
+  /*
+  2. Loops
+  --------
+  "for loop" The for loop executes the code block for a specified number of times
+  "while Loop" evaluates the condition before the block of code is executed.
+  "dowhile Loop" The dowhile loop is similar to the while loop except that the do...while loop doesnt evaluate the 
+  condition for the first time the loop executes.
+  */
+for (int i = 0; i < 5; i++) {
+  print('i = $i');
+}
+  int count = 0;
+while (count < 3) {
+  print('count = $count');
+  count++;
+}
+int x = 0;
+do {
+  print('x = $x');
+  x++;
+} while (x < 2);
+
+/*
+3.switch statement
+------------------
+checking one value against many constants.*/
+
+var grade = 'B';
+
+  switch (grade) {
+    case 'A':
+      print('Excellent!');
+      break;
+    case 'B':
+      print('Well done!');
+      break;
+    case 'C':
+      print('Good');
+      break;
+    default:
+      print('Invalid grade');
+  }
+
+}
---
+++ 22RP03758/operators.dart
@@ -0,0 +1,40 @@
+/*operators are symbols or special keywords used to perform operations on variables and values*/
+
+void main(){
+  var a =10;
+  var b = 5;
+  var c = 12;
+
+  var result;
+  //Arthmetic operator mathematica expressions
+  result = a + b;
+  result = a-b;
+  result = a*b;
+  result =a/b;
+  result =a%b;
+
+  // Equality and Relational Operators
+  result = a > b;
+  result = a < b;
+  result = a >= b;
+  result =a <= b;
+  result =a == b;
+  result =a != b;
+
+  //Assignment Operators
+  a = b;
+  a += b;
+  a ??= b;
+  a -= b;
+  a *= b;
+  result /= a;
+
+  //Logical Operators
+
+  result = (a<b) && (a>b);
+  result = (a<b) || (a>b);
+  result= b != a;
+
+  //
+
+}
