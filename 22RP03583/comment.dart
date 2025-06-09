ad8007826f6b450e399b5bfc4cce6003a63a4602
Mukarukundo-Sophie <mukaru2022@gmail.com>
2025-06-09 15:28:16 +0300

    second work included with variable,operator,test,control flow and comment

---
+++ 22RP03583/comment.dart
@@ -0,0 +1,17 @@
+void main() {
+  // This is a single-line comment
+  print("single line comment! repreesente with //"); // You can also comment after code on the same line
+
+  /*
+    This is a 
+    multi-line comment
+    It can span multiple lines
+  */
+  print("Multi-line comment above  represented with /* */");
+
+  /// This is a documentation comment (used for generating docs)
+  /// It should describe the next code element
+ print("Documentation comments are special! represented with ///");
+
+  // TODO: This is a comment to remind you to do something later
+}
---
+++ 22RP03583/condition.dart
@@ -0,0 +1,11 @@
+void main(){
+  int  a = 5,b =10;
+  //ternary operator
+  if(a>b){
+    print("a is greater than b\n");
+  }else{
+    print("b is greater than a");
+  }
+
+
+}
