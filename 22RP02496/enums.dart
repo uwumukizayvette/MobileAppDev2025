347cb12e642a6c16f0723b5ead09f2c224f4a1a5
cldnuwase <cldnuwase@gmail.com>
2025-06-09 19:33:40 +0200

    enums

---
+++ 22RP02496/enums.dart
@@ -0,0 +1,8 @@
+//Used to define a set of named constants.
+    enum Weather { sunny, rainy, cloudy }
+
+void main() {
+  Weather today = Weather.sunny;
+  print(today); // Output: Weather.sunny
+}
+
