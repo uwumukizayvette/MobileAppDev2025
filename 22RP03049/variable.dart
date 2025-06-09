f5b40f951af0381bd399484c2cba434907833def
Jonathanyzx <jonathannsengimana121@gmail.com>
2025-06-09 16:05:08 +0200

    this is variable program

---
+++ 22RP03049/variable.dart
@@ -0,0 +1,18 @@
+void main() {
+  // String variable
+  String name = 'Jonathan';
+
+  // Integer variable
+  int age = 25;
+
+  // Double variable
+  double height = 5.9;
+
+  // Boolean variable
+  bool isStudent = true;
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: $height');
+  print('Is Student: $isStudent');
+}
