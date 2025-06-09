7c16744ccebd91d8f66ccd8777c67ccfdf117a6b
manzibahizi <manzibahizibertin@gmail.com>
2025-06-09 13:07:23 +0200

    variables

---
+++ 22RP08209/variables.dart
@@ -0,0 +1,4 @@
+String name = "John";      // Text (e.g., username)  
+int age = 25;              // Whole number (e.g., age)  
+double height = 5.11;      // Decimal (e.g., height in feet)  
+bool isStudent = true;     // True/False (e.g., student status)  
