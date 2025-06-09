638342d7839a5d0e980f23563edf4b4f38772296
charlybizima <charlenneuwera@gmail.com>
2025-06-09 14:45:41 +0200

    variables

---
+++ variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = "Charlenne";
+  int age = 21;
+  double height = 1.65;
+  bool isStudent = true;
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Is Student: $isStudent");
+}
