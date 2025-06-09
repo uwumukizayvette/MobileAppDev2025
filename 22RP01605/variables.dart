b2f260bfa93ebd41e2d3782e492b533a1100b451
= <=>
2025-06-09 16:14:06 +0200

    Core Data Structures & Types

--- 22RP01605/variables.dart
+++ 22RP01605/variables.dart
@@ -2,14 +2,14 @@ void main() {
   // Implicitly declared variable (inferred type)
   var age = 25;
   var name = "John";
-  
+
   // Explicitly declared variables
   int score = 100;
   String city = "New York";
-  
+
   // Final variable (cannot be reassigned)
   final double pi = 3.14;
-  
+
   // Constant variable (must be a compile-time constant)
   const int maxLimit = 50;
 
