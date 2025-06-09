49be89d556c1db4aacc605ced4fc3784deca33d5
Adelegt <delphineumutesi9@gmailmail.com>
2025-06-09 08:31:58 -0400

    creating type to test

---
+++ 22RP02186/variables.dart
@@ -0,0 +1,13 @@
+void main(){
+  String name = "Adelphine";
+  int age = 22;
+  double height = 1.75;
+  bool isStudent = true;
+
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height m");
+  print("Is Student: $isStudent");
+
+
+}
