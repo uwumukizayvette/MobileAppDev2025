647a280b5c47f9c25fce3c87d69ab167f84680c2
Adelegt <delphineumutesi9@gmailmail.com>
2025-06-09 09:09:05 -0400

    comments creation

---
+++ 22RP02186/comments.dart
@@ -0,0 +1,13 @@
+void main() {
+  // This is a single-line comment
+
+  /*
+   This is a multi-line comment
+   explaining the main method
+  */
+
+  print("Hello with comments!");
+
+  /// This is a documentation comment
+  /// Used for describing classes, methods, etc.
+}
