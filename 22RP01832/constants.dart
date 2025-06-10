31b1dd4407a06c2d4520698167f82f05e8f1924a
M-I-Theogene <mbonimpatheogene15@gmail.com>
2025-06-10 08:42:00 +0200

    use of constants

---
+++ 22RP01832/constants.dart
@@ -0,0 +1,10 @@
+void main() {
+  // const: compile-time constant
+  const double pi = 3.14;
+
+  // final: runtime constant
+  final DateTime now = DateTime.now();
+
+  print('PI: $pi');
+  print('Now: $now');
+}
