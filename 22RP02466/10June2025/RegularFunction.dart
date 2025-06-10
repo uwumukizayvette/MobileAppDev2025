920855ed82b93c11a1b4348406f865f7102e3442
CynthiaGihozo12345 <igihozoc16@gmail.com>
2025-06-10 10:18:23 +0200

    RegularFunction

---
+++ 22RP02466/10June2025/RegularFunction.dart
@@ -0,0 +1,17 @@
+//Function With Return Value
+int add(int a, int b) {
+  return a + b;
+}
+//Funcction with no return Value
+void greet(String name) {
+  print('Hello, $name!');
+}
+
+void main() {
+  int result = add(5, 3);
+  print('Sum is $result'); // Output: Sum is 8
+  greet('Alice'); // Output: Hello, Alice!
+}
+//
+
+
