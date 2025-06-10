7138818b24596487ee25702d27255ca25242b27c
M-I-Theogene <mbonimpatheogene15@gmail.com>
2025-06-10 08:44:28 +0200

    use switch

---
+++ 22RP01832/switch.dart
@@ -0,0 +1,18 @@
+void main() {
+  int day = 3;
+
+  // Switch statement for decision making
+  switch (day) {
+    case 1:
+      print('Monday');
+      break;
+    case 2:
+      print('Tuesday');
+      break;
+    case 3:
+      print('Wednesday');
+      break;
+    default:
+      print('Unknown day');
+  }
+}
