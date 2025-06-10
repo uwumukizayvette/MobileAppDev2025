ba328f355ba9476f7980a9c788845413b1826341
M-I-Theogene <mbonimpatheogene15@gmail.com>
2025-06-10 08:44:48 +0200

    use of while loops

---
+++ 22RP01832/while_loops.dart
@@ -0,0 +1,16 @@
+void main() {
+  int count = 0;
+
+  // While loop
+  while (count < 3) {
+    print('Count: $count');
+    count++;
+  }
+
+  // Do-while: runs at least once
+  int num = 0;
+  do {
+    print('Num: $num');
+    num++;
+  } while (num < 2);
+}
